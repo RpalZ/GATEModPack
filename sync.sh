96b8fa9b5154905e5badd927b054594b3fc96be4
RpalZ <72819391+RpalZ@users.noreply.github.com>
2026-03-24 17:27:57 +0300

    Init

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# --- CONFIGURATION ---
+# The "Live" folder where you are playing/modding
+GAME_DIR="$HOME/Documents/curseforge/minecraft/Instances/GATE/"
+
+# Your GitHub Repo folder
+REPO_DIR="$HOME/Desktop/GATEModPack/"
+
+# --- THE CAPTURE ---
+echo "Capturing changes from CurseForge into GitHub repo..."
+
+# Syncing from Game -> Repo
+# We use --update so it only copies files that are NEWER in the game folder
+rsync -av --update "$GAME_DIR/kubejs/" "$REPO_DIR/kubejs/"
+rsync -av --update "$GAME_DIR/config/" "$REPO_DIR/config/"
+rsync -av --update "$GAME_DIR/defaultconfigs/" "$REPO_DIR/defaultconfigs/"
+
+echo "Sync complete. Check 'git status' to see what changed!"
