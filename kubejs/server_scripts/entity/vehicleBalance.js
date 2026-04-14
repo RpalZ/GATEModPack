34b240d48f27db6b5f507989ba5c8a584ebc4cbd
RpalZ <72819391+RpalZ@users.noreply.github.com>
2026-04-14 22:35:24 +0300

    Allthemods

--- kubejs/server_scripts/entity/vehicleBalance.js
+++ kubejs/server_scripts/entity/vehicleBalance.js
@@ -6,7 +6,6 @@ EntityEvents.hurt(event => {
     
     if(!player) return
     
-    // level.tell('EVENT FIRED LOLOLOL')
     let playerNbt = player.getNbt()
 
     let isInVehicle = playerNbt.contains("RootVehicle")
@@ -37,12 +36,72 @@ EntityEvents.hurt(event => {
 
     let damage = event.getDamage()
 
-    vehicleEntity.health -= damage*1.5
+    const vehicleMultiplier = 2.0
+
+
+    const finalDamage = damage*vehicleMultiplier
+
+
+    vehicleEntity.health -= finalDamage
 
     // let utilsnbt = Java.loadClass("dev.latvian.mods.rhino.mod.util.NBTUtils")
+
+
+})
+
+EntityEvents.hurt(event => {
+
+    const level = event.getLevel()
+
+    const source = event.getSource()
+    const sourceEntity = source.getActual()
     
 
-    // level.tell("Boss deal some dmg fr nigga")
+
+    const player = source.getPlayer()
+
+    if(!player) return
+ 
+    const bossEntity = event.getEntity()
+    
+    const bossTags = bossEntity.getEntityType().getTags().map(t => t.location().toString()).toList()
+
+    if(!bossTags.contains("forge:bosses")) return
+
+
+    //nerf if player is in vehicle
+
+    const playerNbt = player.getNbt()
+    const isInVehicle = playerNbt.contains("RootVehicle")
+
+    if(!isInVehicle) return
+
+    // level.tell("yes ")
+
+    const damage = event.getDamage()
+
+    const modifier = 0.5
+
+    const finalDamage = damage * modifier
+
+    // bossEntity.attack(source, finalDamage)
+    // event.damage = finalDamage
+    const newHealth = bossEntity.getHealth() + finalDamage
+
+
+    
+
+    bossEntity.setHealth(newHealth)
+    
+    // level.tell(bossEntity.getHealth())
+    if(bossEntity.getHealth() <= 15){
+        // level.tell("check")
+        // const damageSource = level.damageSources().playerAttack(player)
+        bossEntity.attack(source, 100)
+        bossEntity.kill()
+        // level.tell("killed boss")
+    }
+    // event.setAmount(finalDamage)
 
 
 })
