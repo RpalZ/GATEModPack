da5be1cc276f0938fab377c0574c313a759197e7
RpalZ <72819391+RpalZ@users.noreply.github.com>
2026-06-28 02:08:26 +0300

    withercheck

--- kubejs/server_scripts/ftbQuests/witherDone.js
+++ kubejs/server_scripts/ftbQuests/witherDone.js
@@ -18,4 +18,45 @@ FTBQuestsEvents.completed("7E6BF3EAA8695D04", (event) => {
       .italic()
       .lightPurple(),
   );
+
+  player.stages.add("withercheck")
 });
+
+
+PlayerEvents.tick(event => {
+
+  let tick = event.server.tickCount
+
+  if(tick % 20 !==0 ) return
+
+  let player = event.player
+
+  let hasWitherCheck = player.stages.has("withercheck")
+
+  if(hasWitherCheck) return
+
+  let hasWitherDone = player.stages.has("witherdone")
+
+  if(hasWitherDone) {
+    if (!player.persistentData.getBoolean("isMagic")) {
+    player.setAttributeBaseValue("irons_spellbooks:max_mana", 100);
+    player.setAttributeBaseValue("manaunification:max_mana", 100);
+  }
+
+  player.persistentData.merge({
+    isMagic: true,
+    isTech: true,
+  });
+
+  player.stages.add("skipcuriocheck");
+  player.stages.add(dungeonStage);
+  player.setStatusMessage(
+    Text.of("It feels like you have unlocked many things...")
+      .italic()
+      .lightPurple(),
+  );
+
+  player.stages.add("withercheck")
+
+  }
+})
